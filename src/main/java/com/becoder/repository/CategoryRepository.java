f870face35eb858acdd2e3d08198f1ed43381314
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2025-11-20 11:42:03 +0530

    Dto mapper implementation in category features

--- src/main/java/com/becoder/repository/CategoryRepository.java
+++ src/main/java/com/becoder/repository/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.becoder.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.becoder.entity.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+	List<Category> findByIsActiveTrue();
 
 }
