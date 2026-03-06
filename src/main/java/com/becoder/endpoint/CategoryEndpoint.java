8b4af3dfb68481959a97cf87f0cad16cbefcdd0f
Sambit Kumar Dalai <76642695+sambitkumardalai@users.noreply.github.com>
2026-03-06 07:37:10 +0530

    constant implement

--- src/main/java/com/becoder/endpoint/CategoryEndpoint.java
+++ src/main/java/com/becoder/endpoint/CategoryEndpoint.java
@@ -10,28 +10,30 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.becoder.dto.CategoryDto;
+import static com.becoder.util.Constants.ROLE_ADMIN;
+import static com.becoder.util.Constants.ROLE_ADMIN_USER;
 
 @RequestMapping("/api/v1/category")
 public interface CategoryEndpoint {
 
     @PostMapping("/save")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize(ROLE_ADMIN)
     public ResponseEntity<?> saveCategory(@RequestBody CategoryDto categoryDto);
 
     @GetMapping("/")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize(ROLE_ADMIN)
     public ResponseEntity<?> getAllCategory();
 
     @GetMapping("/active")
-    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    @PreAuthorize(ROLE_ADMIN_USER)
     public ResponseEntity<?> getActiveCategory();
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize(ROLE_ADMIN)
     public ResponseEntity<?> getCategortDetailsById(@PathVariable Integer id) throws Exception;
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize(ROLE_ADMIN)
     public ResponseEntity<?> deleteCategoryById(@PathVariable Integer id);
 
 }
--- src/main/java/com/becoder/util/Constants.java
+++ src/main/java/com/becoder/util/Constants.java
@@ -4,5 +4,11 @@ public class Constants {
 
 	public static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
 	public static final String MOBNO_REGEX = "^\\+?[1-9]\\d{9,14}$";
+	public static final String ROLE_ADMIN = "hasRole('ADMIN')";
+	public static final String ROLE_ADMIN_USER = "hasAnyRole('USER','ADMIN')";
+	public static final String ROLE_USER = "hasRole('USER')";
+
+	public static final String DEFAULT_PAGE_NO = "0";
+	public static final String DEFAULT_PAGE_SIZE = "10";
 
 }
